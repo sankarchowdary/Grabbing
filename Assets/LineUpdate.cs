abec114c4c1e09f71c94f1c7464f4b652cd4d3ba
shankar <111725769+sankarchowdary@users.noreply.github.com>
2023-07-20 01:29:27 +0530

    Grabbing
    Grabbing

---
+++ Assets/LineUpdate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+public class LineUpdate : MonoBehaviour
+{
+
+    public Transform StartPos;
+    public Transform EndPos;
+    LineRenderer Line;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Line = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 SPos = StartPos.position;
+        Vector3 EPos = EndPos.position;
+        Line.SetPosition(0, SPos);
+
+        Line.SetPosition(1, EPos);
+    }
+}
---
+++ Assets/sdsd.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class sdsd : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public float speed;
+
+    void FixedUpdate()
+    {
+        //GetComponent<Rigidbody>().AddRelativeForce ((Vector3.right * 1 * speed) - GetComponent<Rigidbody>().velocity);
+        GetComponent<Rigidbody>().AddForce(transform.right * speed, ForceMode.Acceleration);
+    }
+}
